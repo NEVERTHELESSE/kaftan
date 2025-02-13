34e05bfd6f09d126ac99a87dfad331e3a360d8ae
NEVERTHELESSE <neverthelesse@localhost.localdomain>
2025-02-13 15:25:50 +0100

    w

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Router, Route, Link } from "svelte-routing";
+  import Home from "./pages/home/Home.svelte";
+  import About from "./pages/about/About.svelte";
+</script>
+
+<Router>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+  </nav>
+
+  <Route path="/" component={Home} />
+  <Route path="/kaftan" component={Home} />
+  <Route path="/about" component={About} />
+</Router>
---
+++ src/pages/about/About.svelte
@@ -0,0 +1,8 @@
+<script>
+  import girl from '../../assets/girls/girl1.jpg'
+
+</script>
+
+<main>
+  <img src={girl} alt="img" style="width: 20rem;">
+</main>
